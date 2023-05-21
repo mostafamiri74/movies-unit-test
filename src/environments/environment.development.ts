a4864687a85e62fa6b0ddddebd7cae2bd16cffa5
mostafa miri <mostafamiri12345@gmail.com>
2023-05-21 16:13:05 +0330

    Add genres id to env

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -2,4 +2,8 @@ export const environment = {
   production: false,
 
   baseUrl: 'https://moviesapi.ir/api/v1',
+
+  crimeGenresId: 1,
+  actionGenresId: 3,
+  dramaGenresId: 4,
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,4 +2,8 @@ export const environment = {
   production: true,
 
   baseUrl: 'https://moviesapi.ir/api/v1',
+
+  crimeGenresId: 1,
+  actionGenresId: 3,
+  dramaGenresId: 4,
 };
